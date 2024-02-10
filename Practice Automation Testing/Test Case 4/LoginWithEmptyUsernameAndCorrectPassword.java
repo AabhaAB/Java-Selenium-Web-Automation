2a44fc95c80cd12a1c71f3db2f9f459c53031e30
AabhaAB <133474263+AabhaAB@users.noreply.github.com>
2024-02-10 11:10:03 +0530

    Add files via upload

---
+++ Practice Automation Testing/Test Case 4/LoginWithEmptyUsernameAndCorrectPassword.java
@@ -0,0 +1,28 @@
+package PracticeAutomationTesting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginWithEmptyUsernameAndCorrectPassword {
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		
+		ChromeDriver driver = new ChromeDriver();  //Open the browser
+		
+		driver.get("http://practice.automationtesting.in/");
+		
+		driver.manage().window().maximize(); 
+		
+		driver.findElement(By.partialLinkText("Account")).click();   //Click on My Account Menu
+				
+		driver.findElement(By.name("password")).sendKeys("A1@b2@c3@"); //Correct password
+		
+		driver.findElement(By.xpath("//*[@id=\"customer_login\"]/div[1]/form/p[3]/input[3]")).click();  //Click on login button
+	}
+
+}
