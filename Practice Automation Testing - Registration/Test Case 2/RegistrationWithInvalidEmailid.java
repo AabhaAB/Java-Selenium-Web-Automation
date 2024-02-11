8605a1a22fb69463bbbbdd591fbf26e347d95b9e
AabhaAB <133474263+AabhaAB@users.noreply.github.com>
2024-02-11 11:11:40 +0530

    Add files via upload

---
+++ Practice Automation Testing - Registration/Test Case 2/RegistrationWithInvalidEmailid.java
@@ -0,0 +1,30 @@
+package PracticeAutomationTestingRegistration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class RegistrationWithInvalidEmailid {
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		
+		ChromeDriver driver = new ChromeDriver(); 
+		
+		driver.get("http://practice.automationtesting.in/");
+		
+		driver.manage().window().maximize(); 
+		
+		driver.findElement(By.partialLinkText("Account")).click();   //Click on My Account Menu
+		
+		driver.findElement(By.xpath("//*[@id=\"reg_email\"]")).sendKeys("a1b2c3d4@gmail"); //Enter Email address
+		
+		driver.findElement(By.xpath("//*[@id=\"reg_password\"]")).sendKeys("A1@b2@c3@d4@"); //Enter password
+		
+		driver.findElement(By.xpath("//*[@id=\"customer_login\"]/div[2]/form/p[3]/input[3]")).click();  //Click on Register button
+
+	}
+
+}
